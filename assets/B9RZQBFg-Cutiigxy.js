3c2793bdaa7bd0e602ebd75d0d69bc3d5a6cf985
chuawh <chuawh.74@gmail.com>
2025-09-23 13:55:12 +0800

    first commit

---
+++ assets/B9RZQBFg-Cutiigxy.js
@@ -0,0 +1,1 @@
+import{r as t,E as c}from"./index-BnqDZLHb.js";const o=({label:a="No channels",...e})=>t.createElement(c,{icon:"chat",label:a,...e});export{o as default};
---
+++ assets/DJ2cswO9-DNhuSVo9.js
@@ -0,0 +1,1 @@
+import{r as s,E as t}from"./index-BnqDZLHb.js";const r=({label:e="No messages",...a})=>s.createElement(t,{icon:"message",label:e,...a});export{r as default};
